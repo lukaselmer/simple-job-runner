5a320906ba27001c4d12b877a6fb87ab33f0127d
Lukas Elmer <lukas.elmer@gmail.com>
2015-06-21 06:50:53 +0200

    more specs

--- spec/requests/runs_spec.rb
+++ spec/requests/runs_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe 'Runs', type: :request do
       get runs_path(api_key: ENV['API_KEY'])
       expect(response).to have_http_status(200)
     end
+
+    it 'doesnt get the runs when not authenticated' do
+      get runs_path
+      expect(response).to have_http_status(401)
+    end
+
   end
 end
