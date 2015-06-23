54afde1ba0e4f7fe07b06699a4d556c23a4b6119
Lukas Elmer <lukas.elmer@gmail.com>
2015-06-23 14:48:11 +0200

    filter by created_at

--- spec/views/runs/index.html.slim_spec.rb
+++ spec/views/runs/index.html.slim_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'runs/index', type: :view do
   before(:each) do
-    assign(:runs, [
-      build_stubbed(:ended_run, score: 99),
-      build_stubbed(:ended_run, score: 34.5)
-    ])
+    @run_1 = build_stubbed(:ended_run, score: 99, created_at: 25.days.ago)
+    @run_2 = build_stubbed(:ended_run, score: 34.5, created_at: 23.days.ago)
+    assign(:runs, [@run_1, @run_2])
   end
 
   it 'renders a list of runs' do
@@ -14,5 +13,7 @@ RSpec.describe 'runs/index', type: :view do
     assert_select 'tr>td', text: "Blablabla\nScore: 12.254%\nBlabla", count: 2
     assert_select 'tr>td', text: '99.0', count: 1
     assert_select 'tr>td', text: '34.5', count: 1
+    expect(rendered).to include('25 days ago')
+    expect(rendered).to include('23 days ago')
   end
 end
